2aa939537a3222affba6470f9556fe036378e81d
Indra <indra@imceria.com>
2020-09-17 11:01:37 +0700

    Update Style

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -48,7 +48,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="@yield("url")" class="brand-link">
-      <img src="{{url("assets/logo/logo.png")}}" alt="SMK Kesahatan Rajawali" class="brand-image img-circle elevation-3"
+      <img src="{{url("assets/logo/logo.png")}}" alt="SD Aulady" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">E-Learning</span>
     </a>
@@ -90,7 +90,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     E-Learning
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; {{date("Y")}} <a href="{{url("")}}">SMK Kesehatan Rajawali</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; {{date("Y")}} <a href="{{url("")}}">SD Aulady</a>.</strong> All rights reserved.
   </footer>
 </div>
 <!-- ./wrapper -->
