f89872bc499fb850e667d45c6eab36b27e9079d8
Indra Gunanda <indra.gunanda@gmail.com>
2021-02-03 09:52:38 +0700

    print absensi

--- resources/views/guru/virtual/detail.blade.php
+++ resources/views/guru/virtual/detail.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 @section('title',$title)
 @section('css')
-
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
 @endsection
 @section('url',session()->get("url"))
 @section('konten')
@@ -46,9 +46,22 @@
 @endsection
 
 @section('js')
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js" ></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js" ></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js" ></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js" ></script>
     <script type="text/javascript">
         $(document).ready(function (){
-            $("table").DataTable();
+            console.log("Duar")
+            $('table').DataTable( {
+                dom: 'Bfrtip',
+                buttons: [
+                    'pdf'
+                ]
+            } );
         })
     </script>
 @endsection
